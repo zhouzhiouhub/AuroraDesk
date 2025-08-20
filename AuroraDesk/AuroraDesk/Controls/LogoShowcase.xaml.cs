1f7901d97fa6f1ab821c0f27637d63eacbfa312b
ZhouZhenYun <2922188469@qq.com>
2025-08-20 09:25:04 +0800

    修复运行时的WMC9999 表示缺少根元素的bug

---
+++ AuroraDesk/AuroraDesk/Controls/LogoShowcase.xaml.cs
@@ -0,0 +1,16 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace AuroraDesk.Controls
+{
+    /// <summary>
+    /// LogoShowcase 用户控件
+    /// </summary>
+    public sealed partial class LogoShowcase : UserControl
+    {
+        public LogoShowcase()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
