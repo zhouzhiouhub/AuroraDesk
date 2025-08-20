cb9ad58f37bd1ba80e2726d5e46586f4851d9405
ZhouZhenYun <2922188469@qq.com>
2025-08-20 09:11:48 +0800

    添加静态壁纸

--- AuroraDesk/AuroraDesk/obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/AuroraDesk.AssemblyInfo.cs
+++ AuroraDesk/AuroraDesk/obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/AuroraDesk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuroraDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8682ea45b541b286d203237914ec474ccbb8d14a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1682019b3c12b6e155bf91d637b588638a0b3dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuroraDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuroraDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
